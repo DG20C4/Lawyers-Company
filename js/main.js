6763b76bdc35bc2c80d62e8e4f05482b93922f30
David Chaves Gutierrez <142677288+DG20C4@users.noreply.github.com>
2024-11-15 05:39:49 +0000

    new js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const id = document.getElementById('id').value;
-    const email = document.getElementById('email').value;
-    
-    alert(`Nombre: ${name}\nID: ${id}\nCorreo: ${email}`);
-});
